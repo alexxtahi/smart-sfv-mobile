640c22777a61359060a8c6e7e9a7161e4b56c3a0
AlexTAHI <62557152+AlexTAHI@users.noreply.github.com>
2021-08-02 15:02:01 +0000

    Laravel API configurations - PART 1

---
+++ lib/api.dart
@@ -0,0 +1,83 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class Api {
+  // todo: Properties
+  late http.Response response;
+  bool isLinkCorrect = false;
+  late String url;
+
+  // ! App context methods
+  // todo: get user datas method
+  Future getUserData() async {
+    this.url = 'http://192.168.1.16:8000/api/getusers'; // set url
+    this.response = await http.get(Uri.parse(url)); // getting datas from url
+    print('le lien est: $url');
+
+    if (this.response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      this.isLinkCorrect = true;
+      print(this.response);
+      //return models.Food.fromJson(jsonDecode(this.response.body));
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      this.isLinkCorrect = false;
+      throw Exception('Failed to load Food');
+    }
+  }
+
+  // ! End app context methods
+
+  // todo: get data method
+  Future getData() async {
+    this.url = 'http://192.168.138.11:8000/foods'; // set url
+    this.response = await http.get(Uri.parse(url)); // getting datas from url
+    print('le lien est: $url');
+
+    if (this.response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      this.isLinkCorrect = true;
+      print(this.response);
+      //return models.Food.fromJson(jsonDecode(this.response.body));
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      this.isLinkCorrect = false;
+      throw Exception('Failed to load Food');
+    }
+  }
+
+  // todo: post data method
+  void postData(String title, String filename) async {
+    /*
+    ! Best function !!!
+    * This function is use to send data from the flutter app to an API
+    * Add the datas you want to send in the 'body' with key and value.
+    */
+    this.url = 'http://192.168.138.11:8000/foods'; // set url
+    final response = await http.post(
+      Uri.parse(this.url),
+      //headers: {AuthUtils.AUTH_HEADER: _authToken},
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Charset': 'utf-8',
+      },
+      body: {
+        'title': title,
+        'filename': filename,
+        //'image': image != null ? base64Encode(image.readAsBytesSync()) : '',
+      },
+    );
+
+    final responseJson = json.decode(json.encode(response.body));
+
+    print("Réponse du server: $responseJson");
+  }
+}
+
+Api api = new Api();
