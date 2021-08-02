0722adf60cf97d459b5689f6e107525de302e034
AlexTAHI <62557152+AlexTAHI@users.noreply.github.com>
2021-08-02 10:21:19 +0000

    Finalization of DashboardScreen

--- lib/views/components/MyExpandableBox.dart
+++ lib/views/components/MyExpandableBox.dart
@@ -21,6 +21,8 @@ class MyExpandableBox extends StatefulWidget {
 class MyExpandableBoxState extends State<MyExpandableBox> {
   // Scroll controller
   ScrollController scrollController = new ScrollController();
+  // Expandable controller
+  ExpandableController expandableController = new ExpandableController();
   @override
   Widget build(BuildContext context) {
     List<double> screenSize = ScreenController.getScreenSize(context);
@@ -33,144 +35,127 @@ class MyExpandableBoxState extends State<MyExpandableBox> {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15),
         ),
-        child: Expandable(
-          // <-- Driven by ExpandableController from ExpandableNotifier
-          //todo: Collapsed Box
-          collapsed: ExpandableButton(
-            // <-- Expands when tapped on the cover photo
-            child: Container(
-              width: screenSize[0],
-              height: 50,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Flexible(
-                      child: MyText(
-                        text: widget.headerText,
-                        color: Colors.black,
-                        fontSize: 16,
-                      ),
-                    ),
-                    Icon(
-                      Icons.keyboard_arrow_left_rounded,
-                      color: Colors.black,
-                    ),
-                  ],
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Container(
+                alignment: Alignment.center,
+                width: screenSize[0],
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
                 ),
-              ),
-            ),
-          ),
-          //todo: Expanded Box
-          expanded: Row(
-            children: [
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15),
+                child: ExpandablePanel(
+                  controller: expandableController,
+                  theme: ExpandableThemeData(
+                    tapHeaderToExpand: true,
+                    iconPadding:
+                        EdgeInsets.symmetric(horizontal: 15, vertical: 14),
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 15, vertical: 10),
-                    child: Column(
+                  header: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                    alignment: Alignment.centerLeft,
+                    child: Row(
                       children: [
-                        //todo: Header
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Flexible(
-                              child: Text(
-                                widget.headerText,
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                  fontFamily: 'Montserrat',
-                                  color: Colors.black,
-                                  fontSize: 16,
-                                ),
-                              ),
-                            ),
-                            ExpandableButton(
-                              // <-- Collapses when tapped on
-                              child: Icon(
-                                Icons.keyboard_arrow_down_rounded,
-                                color: Colors.black,
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          width: screenSize[0],
-                          height: 2,
-                          child: Container(
-                            color: Color.fromRGBO(60, 141, 188, 1),
+                        Flexible(
+                          child: MyText(
+                            text: widget.headerText,
+                            color: Colors.black,
+                            fontSize: 16,
                           ),
-                        ),
-                        //todo: Content
-                        FadingEdgeScrollView.fromSingleChildScrollView(
-                          gradientFractionOnStart: 0.2,
-                          gradientFractionOnEnd: 0.2,
-                          child: SingleChildScrollView(
-                            controller: scrollController,
-                            physics: BouncingScrollPhysics(),
-                            scrollDirection: Axis.horizontal,
-                            child: widget.table,
-                          ),
-                        ),
-                        //todo: See more button
-                        Row(
-                          children: [
-                            Spacer(),
-                            ElevatedButton(
-                              onPressed: () {},
-                              child: Row(
-                                children: [
-                                  //todo: Text
-                                  MyText(
-                                    text: 'Voir plus',
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w800,
-                                    fontSize: 12,
-                                  ),
-                                  SizedBox(width: 5),
-                                  //todo: Icon
-                                  Image.asset(
-                                    'assets/img/icons/previous.png',
-                                    width: 20,
-                                    height: 20,
-                                    fit: BoxFit.contain,
-                                    color: Colors.white,
-                                  ),
-                                ],
-                              ),
-                              style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all<Color>(
-                                        Color.fromRGBO(60, 141, 188, 1)),
-                                shape: MaterialStateProperty.all<
-                                    RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
                         ),
                       ],
                     ),
                   ),
+                  //todo: Collapsed Box
+                  collapsed: Container(),
+                  //todo: Expanded Box
+                  expanded: Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          child: Column(
+                            children: [
+                              //todo: Separator
+                              SizedBox(
+                                width: screenSize[0],
+                                height: 2,
+                                child: Container(
+                                  color: Color.fromRGBO(60, 141, 188, 1),
+                                ),
+                              ),
+                              //todo: Content
+                              FadingEdgeScrollView.fromSingleChildScrollView(
+                                gradientFractionOnStart: 0.2,
+                                gradientFractionOnEnd: 0.2,
+                                child: SingleChildScrollView(
+                                  controller: scrollController,
+                                  physics: BouncingScrollPhysics(),
+                                  scrollDirection: Axis.horizontal,
+                                  child: widget.table,
+                                ),
+                              ),
+                              //todo: See more button
+                              Padding(
+                                padding: EdgeInsets.only(
+                                  bottom: 5,
+                                  left: 10,
+                                  right: 10,
+                                ),
+                                child: Row(
+                                  children: [
+                                    Spacer(),
+                                    ElevatedButton(
+                                      onPressed: () {},
+                                      child: Row(
+                                        children: [
+                                          //todo: Text
+                                          MyText(
+                                            text: 'Voir plus',
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w800,
+                                            fontSize: 12,
+                                          ),
+                                          SizedBox(width: 5),
+                                          //todo: Icon
+                                          Image.asset(
+                                            'assets/img/icons/previous.png',
+                                            width: 20,
+                                            height: 20,
+                                            fit: BoxFit.contain,
+                                            color: Colors.white,
+                                          ),
+                                        ],
+                                      ),
+                                      style: ButtonStyle(
+                                        backgroundColor:
+                                            MaterialStateProperty.all<Color>(
+                                                Color.fromRGBO(
+                                                    60, 141, 188, 1)),
+                                        shape: MaterialStateProperty.all<
+                                            RoundedRectangleBorder>(
+                                          RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
